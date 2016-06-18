70300a1b9c0eb65d79a9fd47a2220b527fedacb7
machonky <dev@macrobert.com>
2016-06-18 15:12:33 +0800

    Description added

--- CoreMemoryBus/Properties/AssemblyInfo.cs
+++ CoreMemoryBus/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CoreMemoryBus")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple and configurable, in-memory message bus for message based event driven applications.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MacRobert.com")]
 [assembly: AssemblyProduct("CoreMemoryBus")]
